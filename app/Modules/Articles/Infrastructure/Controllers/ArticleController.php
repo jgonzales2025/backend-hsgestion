b1807f933787523a91f7af286e77889ec3d56fff
carlos <carlos.rojas.ramirez.25@gmail.com>
2025-10-31 12:37:23 -0500

    --

--- app/Modules/Articles/Infrastructure/Controllers/ArticleController.php
+++ app/Modules/Articles/Infrastructure/Controllers/ArticleController.php
@@ -41,33 +41,29 @@ class ArticleController extends Controller
     private readonly CurrencyTypeRepositoryInterface $currencyTypeRepository,
     private readonly SubCategoryRepositoryInterface $subCategoryRepository,
     private readonly CompanyRepositoryInterface $companyRepository,
-    //  private ExportArticlesToExcelUseCase $exportUseCase
+      // private ExportArticlesToExcelUseCase $exportUseCase
 
   ) {
   }
-  //  public function export(Request $request)
-  //   {
-  //       $validated = $request->validate([
-  //           'id' => 'required|int|min:1',
-  //       ]);
-
-  //       try {
-  //           $filePath = $this->exportUseCase->execute($validated['id']);
-            
-  //           return response()->download(
-  //               storage_path('app/public/' . $filePath),
-  //               basename($filePath),
-  //               [
-  //                   'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  //               ]
-  //           )->deleteFileAfterSend(true);
-            
-  //       } catch (\Exception $e) {
-  //           return response()->json([
-  //               'error' => $e->getMessage()
-  //           ], $e->getMessage() === 'No se encontraron artículos con los IDs proporcionados' ? 404 : 500);
-  //       }
-  //   }
+// public function export(int $id)
+// {
+//     try {
+//         $filePath = $this->exportUseCase->execute($id);
+        
+//         return response()->download(
+//             storage_path('app/public/' . $filePath),
+//             basename($filePath),
+//             [
+//                 'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+//             ]
+//         )->deleteFileAfterSend(true);
+        
+//     } catch (\Exception $e) {
+//         return response()->json([
+//             'error' => $e->getMessage()
+//         ], 404);
+//     }
+// }
 
   public function index(Request $request): array
   {

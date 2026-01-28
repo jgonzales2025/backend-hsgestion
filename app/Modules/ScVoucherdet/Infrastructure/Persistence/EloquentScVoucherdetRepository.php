95b35ac584293f73319c8d027493bd8b09079311
carlos <carlos.rojas.ramirez.25@gmail.com>
2026-01-28 11:20:55 -0500

    feat: ---

--- app/Modules/ScVoucherdet/Infrastructure/Persistence/EloquentScVoucherdetRepository.php
+++ app/Modules/ScVoucherdet/Infrastructure/Persistence/EloquentScVoucherdetRepository.php
@@ -10,14 +10,12 @@ class EloquentScVoucherdetRepository implements ScVoucherdetRepositoryInterface
 {
     public function create(ScVoucherdet $scVoucherdet): ?ScVoucherdet
     {
-        $scVoucherdet = EloquentScVoucherdet::create([
+        $model = EloquentScVoucherdet::create([
             'cia' => $scVoucherdet->getCia(),
-            'codcon' => $scVoucherdet->getCodcon(),
+            'codcon' => $scVoucherdet->getCodcon()?->getId(),
             'tipdoc' => $scVoucherdet->getTipdoc(),
-
             'glosa' => $scVoucherdet->getGlosa(),
             'impsol' => $scVoucherdet->getImpsol(),
-            'impdol' => $scVoucherdet->getImpdol(),
             'impdol' => $scVoucherdet->getImpdol(),
             'id_purchase' => $scVoucherdet->getIdPurchase(),
             'id_sc_voucher' => $scVoucherdet->getIdScVoucher(),
@@ -26,31 +24,21 @@ class EloquentScVoucherdetRepository implements ScVoucherdetRepositoryInterface
             'serie' => $scVoucherdet->getSerie(),
         ]);
 
-        return new ScVoucherdet(
-            id: $scVoucherdet->id,
-            cia: $scVoucherdet->cia,
-            codcon: $scVoucherdet->codcon,
-            tipdoc: $scVoucherdet->tipdoc,
-
-            glosa: $scVoucherdet->glosa,
-            impsol: $scVoucherdet->impsol,
-            impdol: $scVoucherdet->impdol,
-            id_purchase: $scVoucherdet->id_purchase,
-            id_sc_voucher: $scVoucherdet->id_sc_voucher,
-            numdoc: $scVoucherdet->numdoc,
-            correlativo: $scVoucherdet->correlativo,
-            serie: $scVoucherdet->serie,
-        );
+        return $model->load('paymentConcept')->toDomain();
     }
+
     public function update(ScVoucherdet $scVoucherdet): ?ScVoucherdet
     {
-        $scVoucherdet = EloquentScVoucherdet::find($scVoucherdet->getId());
+        $model = EloquentScVoucherdet::find($scVoucherdet->getId());
 
-        $scVoucherdetA = $scVoucherdet->update([
+        if (!$model) {
+            return null;
+        }
+
+        $model->update([
             'cia' => $scVoucherdet->getCia(),
-            'codcon' => $scVoucherdet->getCodcon(),
+            'codcon' => $scVoucherdet->getCodcon()?->getId(),
             'tipdoc' => $scVoucherdet->getTipdoc(),
-
             'glosa' => $scVoucherdet->getGlosa(),
             'impsol' => $scVoucherdet->getImpsol(),
             'impdol' => $scVoucherdet->getImpdol(),
@@ -60,84 +48,28 @@ class EloquentScVoucherdetRepository implements ScVoucherdetRepositoryInterface
             'serie' => $scVoucherdet->getSerie(),
         ]);
 
-        return new ScVoucherdet(
-            id: $scVoucherdetA->id,
-            cia: $scVoucherdetA->cia,
-            codcon: $scVoucherdetA->codcon,
-            tipdoc: $scVoucherdetA->tipdoc,
-
-            glosa: $scVoucherdetA->glosa,
-            impsol: $scVoucherdetA->impsol,
-            impdol: $scVoucherdetA->impdol,
-            id_purchase: $scVoucherdetA->id_purchase,
-            id_sc_voucher: $scVoucherdetA->id_sc_voucher,
-            numdoc: $scVoucherdet->numdoc,
-            correlativo: $scVoucherdet->correlativo,
-            serie: $scVoucherdet->serie,
-        );
+        return $model->load('paymentConcept')->toDomain();
     }
 
     public function findById(int $id): ?ScVoucherdet
     {
-        $scVoucherdet = EloquentScVoucherdet::find($id);
-        return new ScVoucherdet(
-            id: $scVoucherdet->id,
-            cia: $scVoucherdet->cia,
-            codcon: $scVoucherdet->codcon,
-            tipdoc: $scVoucherdet->tipdoc,
-
-            glosa: $scVoucherdet->glosa,
-            impsol: $scVoucherdet->impsol,
-            impdol: $scVoucherdet->impdol,
-            id_purchase: $scVoucherdet->id_purchase,
-            id_sc_voucher: $scVoucherdet->id_sc_voucher,
-            numdoc: $scVoucherdet->numdoc,
-            correlativo: $scVoucherdet->correlativo,
-            serie: $scVoucherdet->serie,
-        );
+        $model = EloquentScVoucherdet::with('paymentConcept')->find($id);
+        return $model ? $model->toDomain() : null;
     }
     public function findAll(): array
     {
-        $scVoucherdet = EloquentScVoucherdet::all();
-        return $scVoucherdet->map(function ($scVoucherdet) {
-            return new ScVoucherdet(
-                id: $scVoucherdet->id,
-                cia: $scVoucherdet->cia,
-                codcon: $scVoucherdet->codcon,
-                tipdoc: $scVoucherdet->tipdoc,
-
-                glosa: $scVoucherdet->glosa,
-                impsol: $scVoucherdet->impsol,
-                impdol: $scVoucherdet->impdol,
-                id_purchase: $scVoucherdet->id_purchase,
-                id_sc_voucher: $scVoucherdet->id_sc_voucher,
-                numdoc: $scVoucherdet->numdoc,
-                correlativo: $scVoucherdet->correlativo,
-                serie: $scVoucherdet->serie,
-            );
-        })->toArray();
+        return EloquentScVoucherdet::with('paymentConcept')->get()
+            ->map(fn($model) => $model->toDomain())
+            ->toArray();
     }
 
     public function findByVoucherId(int $voucherId): array
     {
-        $scVoucherdetCollection = EloquentScVoucherdet::where('id_sc_voucher', $voucherId)->get();
-
-        return $scVoucherdetCollection->map(function ($scVoucherdet) {
-            return new ScVoucherdet(
-                id: $scVoucherdet->id,
-                cia: $scVoucherdet->cia,
-                codcon: $scVoucherdet->codcon,
-                tipdoc: $scVoucherdet->tipdoc,
-                glosa: $scVoucherdet->glosa,
-                impsol: $scVoucherdet->impsol,
-                impdol: $scVoucherdet->impdol,
-                id_purchase: $scVoucherdet->id_purchase,
-                id_sc_voucher: $scVoucherdet->id_sc_voucher,
-                numdoc: $scVoucherdet->numdoc,
-                correlativo: $scVoucherdet->correlativo,
-                serie: $scVoucherdet->serie,
-            );
-        })->toArray();
+        return EloquentScVoucherdet::with('paymentConcept')
+            ->where('id_sc_voucher', $voucherId)
+            ->get()
+            ->map(fn($model) => $model->toDomain())
+            ->toArray();
     }
 
     public function deleteByVoucherId(int $voucherId): void
@@ -146,48 +78,19 @@ class EloquentScVoucherdetRepository implements ScVoucherdetRepositoryInterface
     }
     public function getvoucherPurchase(int $id): array
     {
-        $voucherPurchase = EloquentScVoucherdet::where('id_purchase', $id)->get();
-
-        return  $voucherPurchase->map(function ($voucherPurchase) {
-            return new ScVoucherdet(
-                id: $voucherPurchase->id,
-                cia: $voucherPurchase->cia,
-                codcon: $voucherPurchase->codcon,
-                tipdoc: $voucherPurchase->tipdoc,
-                glosa: $voucherPurchase->glosa,
-                impsol: $voucherPurchase->impsol,
-                impdol: $voucherPurchase->impdol,
-                id_purchase: $voucherPurchase->id_purchase,
-                id_sc_voucher: $voucherPurchase->id_sc_voucher,
-                numdoc: $voucherPurchase->numdoc,
-                correlativo: $voucherPurchase->correlativo,
-                serie: $voucherPurchase->serie,
-            );
-        })->toArray();
+        return EloquentScVoucherdet::with('paymentConcept')
+            ->where('id_purchase', $id)
+            ->get()
+            ->map(fn($model) => $model->toDomain())
+            ->toArray();
     }
     public function findDetailByVoucherAndPurchase(int $voucherId, int $purchaseId): ?ScVoucherdet
     {
-        $scVoucherdet = EloquentScVoucherdet::where('id_sc_voucher', $voucherId)
+        $model = EloquentScVoucherdet::with('paymentConcept')
+            ->where('id_sc_voucher', $voucherId)
             ->where('id_purchase', $purchaseId)
             ->first();
 
-        if (!$scVoucherdet) {
-            return null;
-        }
-
-        return new ScVoucherdet(
-            id: $scVoucherdet->id,
-            cia: $scVoucherdet->cia,
-            codcon: $scVoucherdet->codcon,
-            tipdoc: $scVoucherdet->tipdoc,
-            glosa: $scVoucherdet->glosa,
-            impsol: $scVoucherdet->impsol,
-            impdol: $scVoucherdet->impdol,
-            id_purchase: $scVoucherdet->id_purchase,
-            id_sc_voucher: $scVoucherdet->id_sc_voucher,
-            numdoc: $scVoucherdet->numdoc,
-            correlativo: $scVoucherdet->correlativo,
-            serie: $scVoucherdet->serie,
-        );
+        return $model ? $model->toDomain() : null;
     }
 }
